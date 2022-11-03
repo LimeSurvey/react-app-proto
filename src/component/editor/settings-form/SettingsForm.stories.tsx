619c4aebe0b7e3b05b62420e73d5707bf72a6567
Kevin Foster <kevin.foster.uk@gmail.com>
2022-11-03 08:44:10 +0000

    Update settings form story

--- src/component/editor/settings-form/SettingsForm.stories.tsx
+++ src/component/editor/settings-form/SettingsForm.stories.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 
 import './SettingsForm.scss';
 import { SettingsForm } from './SettingsForm';
+import { queryClient, persistOptions } from '../../../model/Query'
 
 export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Editor/SettingsForm',
-  component: SettingsForm,
+    /* 👇 The title prop is optional.
+    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+    * to learn how to generate automatic titles
+    */
+    title: 'Editor/SettingsForm',
+    component: SettingsForm,
 } as ComponentMeta<typeof SettingsForm>;
 
 export const Basic: ComponentStory<typeof SettingsForm> = () => (
-    <SettingsForm />
+    <PersistQueryClientProvider client={queryClient} persistOptions={persistOptions}>
+        <SettingsForm />
+    </PersistQueryClientProvider>
 );
